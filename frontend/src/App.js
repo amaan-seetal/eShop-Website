43220cdcdf017cfca08adbd5f6d4ad8790a412bf
Amaan Seetal <macseetal@gmail.com>
2022-03-07 04:56:51 +0800

    Added Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react"
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to eShop</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to eShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
